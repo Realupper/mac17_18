88a243e2a3f8129aa5bc186b0ac9dca99699d45a
Chris Ortega <realupper@hotmail.com>
2017-11-27 14:49:45 +0100

    Correccion ejercicio hash

--- 13-11-2017/ejercicio_3_pag_128.hs
+++ 13-11-2017/ejercicio_3_pag_128.hs
@@ -5,6 +5,6 @@ divisores y = [x|x<-[1..y], mod y x ==0]
 primo y = divisores y==[1,y]
 
 primos= [x|x<-[1..], primo x]
-nprimos n= [(primos!!m)|m<-[0..n]]
+nprimos n= [(primos!!m)|m<-[0..n-1]]
 
-hashF m= sum[(m!!y)*((nprimos (length(m)))!!y)|y<-[0..length(m)-1]]
+hashF m= sum[(m!!y)*((nprimos (length(m)))!!(length(m)-1-y))|y<-[0..length(m)-1]]
